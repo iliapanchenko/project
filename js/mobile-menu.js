5354815eb5559d44f07a6682b62abd423e2c0bdf
Ilia Panchenko <iliapanchenko@gmail.com>
2026-02-06 13:55:48 +0200

    закінчив адаптацію під мобільні пристрої. Доробив мобільне меню навігації

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,22 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+// Вибираємо всі посилання в меню
+const menuLinks = document.querySelectorAll('.mobile-menu__link');
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('is-open');
+  document.body.classList.toggle('is-scroll-disabled');
+};
 
+// Відкриття та закриття по кнопках
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+// Закриття меню при натисканні на будь-яке посилання
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.classList.remove('is-scroll-disabled');
+  });
+});
